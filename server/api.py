b88da2ab65c1964c847c7b984688d92cf9af3a98
nitinsurya <nitinsurya1991@gmail.com>
2017-06-14 17:33:00 -0500

    Updated api

--- server/api.py
+++ server/api.py
@@ -50,5 +50,5 @@ def getFeatureRequests():
 
 
 if __name__ == '__main__':
-  # app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
-  app.run(debug=True, threaded=True)
+  app.run(host='0.0.0.0', port=8080)
+  #app.run(debug=True, threaded=True)
